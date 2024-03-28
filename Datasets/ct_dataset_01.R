fd19306ca0c19364b6d49e954d90f8a599bb6115
liyuan-bioinfo <65613804+liyuan-bioinfo@users.noreply.github.com>
2024-03-28 19:52:51 +0800

    Update ct_dataset_01.R

--- Datasets/ct_dataset_01.R
+++ Datasets/ct_dataset_01.R
@@ -10,11 +10,20 @@ library(org.Mm.eg.db)
 setwd("")
 # Imputating missing values with 0.1 * minimum intensity of each protein
 {
+    impute_func2 <- function(flag.tc_lfq_order){
+        min.num <- min(flag.tc_lfq_order[(flag.tc_lfq_order!=0)])/10
+        flag.tc_lfq_order[(flag.tc_lfq_order==0)]=min.num    
+        return(log2(flag.tc_lfq_order))
+  }
+    
+    Obj.list = readRDS(file = "ct_dataset_01.rds")
+    Obj.list$min_10_imputate_log2 = Obj.list$filter
+
+    for(i in 1:dim(Obj.list$min_10_imputate_log2)[1]){
+        Obj.list$min_10_imputate_log2[i,] = impute_func2(Obj.list$min_10_imputate_log2[i,])
+      }
+    row.names(Obj.list$min_10_imputate_log2) = row.names(Obj.list$filter)
+    saveRDS(Obj.list,file = "ct_dataset_01.rds")
     
 }
 
-# Imputating missing values with random distribution for ploting
-{
-
-    
-}
