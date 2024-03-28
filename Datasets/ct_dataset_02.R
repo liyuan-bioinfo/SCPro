bbed8d0a36181b3006855a642e7030a7a1cc29fe
liyuan-bioinfo <65613804+liyuan-bioinfo@users.noreply.github.com>
2024-03-28 19:46:05 +0800

    Update ct_dataset_02.R

--- Datasets/ct_dataset_02.R
+++ Datasets/ct_dataset_02.R
@@ -8,90 +8,73 @@ library(clusterProfiler)
 library(org.Mm.eg.db)
 
 setwd("")
-# Signficance Analysis with one-way ANOVA, for Figure 4c, 4d and 4e.
+
+# Imputating missing values with 0.1 * minimum intensity of each protein
 {
-    rm(list=ls())
-    Obj.list = readRDS(file="sp_dataset_03.rds")
-    regions = c("Acinar","PanIN","PDAC")
-    meta.df = Obj.list$meta %>% dplyr::filter(Group %in% regions)
-        
-    meta.df$Regions = factor(meta.df$Group,levels = regions)
-    meta.df = meta.df %>% arrange(Regions)
-    input.df = Obj.list$impute[,meta.df$SampleID]
+    impute_func2 <- function(flag.tc_lfq_order){
+    min.num <- min(flag.tc_lfq_order[(flag.tc_lfq_order!=0)])/10
+    flag.tc_lfq_order[(flag.tc_lfq_order==0)]=min.num
     
-    Pvalue = c()
-    for(i in 1:dim(input.df)[1]){
-        pid.df = input.df[i,] %>% t() %>% as.data.frame()
-        names(pid.df) = "pid"
-        pid.df$SampleId = row.names(pid.df)
-        pid.df$Region = meta.df$Regions
-        p.value = summary(aov(data=pid.df,pid~Region))[[1]]$`Pr(>F)`[1]
-        Pvalue = c(Pvalue,p.value)
-        
-    }
-    input.df$Pvalue = Pvalue
-    input.df$fdr = p.adjust(Pvalue,method = "BH")
+    return(log2(flag.tc_lfq_order))
+  }
+
+    Obj.list = readRDS(file="ct_dataset_02.rds")
+    Obj.list$min_10_impute_log2 = Obj.list$filter
+    for(i in 1:dim(Obj.list$min_10_imputate_log2)[1]){#2516
+        Obj.list$min_10_impute_log2[i,] = impute_func2(Obj.list$min_10_impute_log2[i,])
+    }  
     
-    median.df = Obj.list$impute_median
-    median.df = median.df %>% dplyr::select(Acinar,PanIN,PDAC)
-    
-    dep.df = data.frame()
-    for(i in 1:3){
-        temp.df = median.df[,i] - median.df[,1:3]
-        temp_pid = names(which(rowSums(temp.df > log2(2)) == 2)) # The fold change with one vs the rest strategy. For figure 4e, should change to log2(1.2)
-        temp.df2 = data.frame(pid=temp_pid)
-        temp.df2$region = names(median.df)[i]
-        temp.df2$log2FC = apply(temp.df[temp_pid,-i],1,median)
-        dep.df = rbind(dep.df,temp.df2)
-        
-    }
-    row.names(dep.df) = dep.df$pid
-    dep.df$genename = Obj.list$anno[dep.df$pid,"Gene"]
-    
-    temp.df = input.df %>% dplyr::select(Pvalue, fdr)
-    temp.df$pid = row.names(temp.df)
-    dep.df = dep.df %>% merge(temp.df,by="pid",all.x=T,all.y=F)#add sig
-    
-    dep.df$region = factor(dep.df$region,levels=regions)
-    dep.df = dep.df %>% arrange(region) # Table 1
-    
-    Obj.list$PDAC_stage = dep.df
-    saveRDS(Obj.list,file = "sp_dataset_03.rds")    
 }
 
-# Signficance Analysis with two-sided Student's t-test, for Figure 4g and 4h.
+# Significance calculate
 {
     rm(list=ls())
-    Obj.list = readRDS(file="write/sp_dataset_03.rds")
-    regions = c("LN","IT")
-    meta.df = Obj.list$meta %>% dplyr::filter(Group %in% regions)
-        
-    meta.df$Regions = factor(meta.df$Group,levels = regions)
-    meta.df = meta.df %>% arrange(Regions)
-    input.df = Obj.list$filter[,meta.df$SampleID]
-    input.df = input.df[-which(rowSums(input.df)==0),]
+    Obj.list = readRDS("ct_dataset_02.rds")
     
-    Pvalue = c()
-    log2FC = c()
-    for(i in 1:dim(input.df)[1]){
-        pid.df = input.df[i,] %>% t() %>% as.data.frame()
-        names(pid.df) = "pid"
-        pid.df$SampleId = row.names(pid.df)
-        pid.df$Region = meta.df$Regions
-        p.value = t.test(data=pid.df,pid~Region)$p.value
-        Pvalue = c(Pvalue,p.value)
-        
-        temp_FC = median(pid.df$pid[1:3]) - median(pid.df$pid[4:6])
-        log2FC = c(log2FC, temp_FC)
-        
+    run_single_comparision = function(groupA,groupB,known_marker){
+      temp_meta <- Obj.list$meta
+      temp_meta = temp_meta[temp_meta$group %in% c(groupA,groupB),]
+      temp_meta$group = factor(temp_meta$group,levels = c(groupA,groupB))
+      temp_meta$cell_classification = NULL
+      temp_meta = temp_meta[order(temp_meta$group),]
+      
+      temp.df = Obj.list$no_imputate_log2
+      temp.df = temp.df %>% select(temp_meta$sample_id)
+      temp.df = temp.df[apply(temp.df,1,sum)!=0,]
+      temp.df = Obj.list$min_10_imputate_log2[row.names(temp.df),temp_meta$sample_id]
+      
+      temp_samples_A = temp_meta$sample_id[which(temp_meta$group==groupA)]
+      temp_samples_B = temp_meta$sample_id[which(temp_meta$group==groupB)]
+      temp_group_A = groupA
+      temp_group_B = groupB
+      
+      temp_meta.select = temp_meta %>% filter(group==temp_group_A | group==temp_group_B)
+      temp_meta.select$group = factor(temp_meta.select$group, levels = c(temp_group_A,temp_group_B))
+      temp.df.select = temp.df %>% select(temp_samples_A,temp_samples_B) #df
+      
+      temp.design <- model.matrix(~temp_meta.select$group)
+      colnames(temp.design) <- levels(temp_meta.select$group)
+      rownames(temp.design) <- temp_meta.select$sample_id
+            
+      fit <- lmFit(temp.df.select, temp.design)
+      fit <- eBayes(fit, trend=TRUE)
+      temp.result.limma <- topTable(fit,  coef=2,n=Inf)
+      
+      temp.df.select = temp.df.select[row.names(temp.result.limma),]
+      temp.df.select$P.Value = temp.result.limma$P.Value
+      temp.df.select$fdr = temp.result.limma$adj.P.Val
+      temp.df.select$logFC = temp.result.limma$logFC
+      temp.df.select$`-log10_P.Value` = -log10(temp.df.select$P.Value)
+      temp.df.select$gene = Obj.list$annotation[row.names(temp.df.select),"genename"]
+      
+      temp.df.select$gene_plot = c()
+      temp_ind = temp.df.select$gene %in% known_marker
+      temp.df.select$gene_plot[temp_ind] = temp.df.select[temp_ind,"gene"]
+      
+      return(temp.df.select)
     }
-    input.df$Pvalue = Pvalue
-    input.df$fdr = p.adjust(Pvalue,method = "BH")
-    input.df$log2FC = log2FC
-    
-    input.df$pid = Obj.list$anno[row.names(input.df),"pid"]
-    input.df$genename = Obj.list$anno[row.names(input.df),"Gene"]
-    
-    Obj.list$Lym_DEP = input.df
-    saveRDS(Obj.list,file = "sp_dataset_03.rds")
+        
+    dep_df = run_single_comparision("KPC_425KN","KPC_425KP",c("Foxp3","Cd4","Ikzf2","Klrg1","Kdelr2","Casp1","Map4k1"))
+    Obj.list$dep_df = dep_df
+    # write.csv(dep_df,file="write/ct_dataset_02_dep.csv")
 }
