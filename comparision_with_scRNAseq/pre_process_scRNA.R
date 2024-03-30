26010b0d8b4ec88ee0edbeb682c04b50a44ee533
liyuan-bioinfo <65613804+liyuan-bioinfo@users.noreply.github.com>
2024-03-30 20:29:01 +0800

    Update pre_process_scRNA.R

--- comparision_with_scRNAseq/pre_process_scRNA.R
+++ comparision_with_scRNAseq/pre_process_scRNA.R
@@ -1,10 +1,8 @@
 # demo file
-#'@author LiYuan
-#'@output CellAnnotation
-#'
-
+#'@author Yuan
 #===============================================================================
-# Part I, Basic Analysis with scRNA-seq data using Seurat
+#           Part I, Basic Analysis with scRNA-seq data using Seurat            #
+#================================================================================
 
 rm(list=ls())
 library(Seurat)
@@ -14,21 +12,21 @@ library(ggplot2)
 library(tidyr)
 library(pheatmap)
 # devtools::install_github("immunogenomics/harmony")
+library(harmony)    
 
 options(stringsAsFactors = F)
 options(encoding = "UTF-8")
 
 setwd("")
-
-#'@author Yuan
-#'@desc scRNA-seq from GSE129455|GSE129455_All_Viable_expression.csv.gz
 {
   rm(list=ls())
   kpc.data <- read.csv("GSE129455_All_Viable_expression.csv",header = TRUE)
   saveRDS(kpc.data,file="GSE129455_All_Viable_expression.rda")  
 }
 
-#scRNA-seq pre-treat
+# scRNA-seq pre-treat
+#'@input GSE129455_All_Viable_expression.rda
+#'@desc rename genename; basic pre-process of scRNA-seq [normalise and scaled]
 {
   temp.KPC.raw = readRDS("GSE129455_All_Viable_expression.rda")
   row.names(temp.KPC.raw)=temp.KPC.raw$X
@@ -105,16 +103,17 @@ setwd("")
   KPC.Seurat.rename$CC.Difference <- KPC.Seurat.rename$S.Score - KPC.Seurat.rename$G2M.Score
   
   # Scale
-  KPC.Seurat.rename.scale<- ScaleData(object = KPC.Seurat.rename,vars.to.regress = c("nCount_RNA","CC.Difference"))
-  
+  KPC.Seurat.rename.scale<- ScaleData(object = KPC.Seurat.rename,vars.to.regress = c("nCount_RNA","CC.Difference"))  
   saveRDS(KPC.Seurat.rename.scale,file="GSE129455_All_Viable_expression_scale_20220913.rda")
 
 }
 
-rm(list=ls())
-#Harmony
-{
-  library(harmony)    
+#===============================================================================
+#                Part II, correct batch effect using Harmony                   #
+#================================================================================
+# remove batch effect using Harmony
+{  
+  rm(list=ls())
   KPC.Seurat = readRDS(file="GSE129455_All_Viable_expression_scale_20220913.rda")
   
   KPC.Seurat = Seurat::RunPCA(KPC.Seurat,features=VariableFeatures(object = KPC.Seurat))
@@ -149,7 +148,8 @@ rm(list=ls())
   
   saveRDS(KPC.Seurat,file="GSE129455_All_Viable_expression_before.rda")
   # load(file="test_out/TestTissue.beforeBatchCorr_202204.RData")
-  ### Batch correction with harmony
+  
+  # Batch correction with harmony
   Idents(object = KPC.Seurat) <- 'sample'
   options(repr.plot.height = 2.5, repr.plot.width = 6)
   KPC.Seurat.harmony <- KPC.Seurat %>% 
@@ -167,9 +167,11 @@ rm(list=ls())
   
 }
 
-rm(list=ls())
-#CellAssign
+#===============================================================================
+#     Part III, Annotation of shared cell types with known markers manually     #
+#================================================================================
 {
+  rm(list=ls())
   TotalTissue.harmony = readRDS("GSE129455_All_Viable_expression_after.rda")
   PCC = c("Epcam", "Krt19", "Mmp7", "Krt8", "Krt18","Cdh1")
   Pericyte=c("Rgs5", "Acta2", "Pdgfrb")
@@ -270,11 +272,6 @@ rm(list=ls())
   df = TotalTissue.harmony@meta.data
   Idents(TotalTissue.harmony) = TotalTissue.harmony$RNA_snn_res.0.5
   current.cluster.ids <- c(1:19)
-  # current.cluster.ids <- c("Mac/Mono","Mac/Mono","Mac/Mono","CD4","NK","Epithelial",
-  #                      "Mac/Mono","Epithelai","Mac/Mono","Mac/Mono","Mac/Mono",
-  #                      "Fibroblast","T/NK","Mac/Mono","DCs","B",
-  #                      "Endothelial","Acinar","Pericyte"
-  #                      )
   current.cluster.ids[c(1,2,3,5,11)]="MAC"
   current.cluster.ids[c(10)]="MO"
   current.cluster.ids[c(9,14,15)]="DC"
@@ -293,55 +290,55 @@ rm(list=ls())
   
   saveRDS(TotalTissue.harmony,file="GSE129455_All_Viable_annotate.rda")
   
-  ##> T cell subset
+  # T cell subset
   {
-  T_NK_cell_subset <- subset(TotalTissue.harmony, idents = "T")
-  
-  T_NK_cell_subset <- FindVariableFeatures(object = T_NK_cell_subset, mean.function = ExpMean, dispersion.function = LogVMR)
-  T_NK_cell_subset <- ScaleData(object = T_NK_cell_subset, vars.to.regress = c("nCount_RNA","CC.Difference"))
-  T_NK_cell_subset <- RunPCA(object = T_NK_cell_subset, pc.genes = T_NK_cell_subset@var.genes, do.print = TRUE, pcs.print = 1:5, genes.print = 5)
-  
-  #Find number of PCs that gives 90% variance
-  st_dev <- T_NK_cell_subset@reductions$pca@stdev
-  var <- st_dev^2
-  sum(var[1:41])/sum(var)
-  #Harmony Batch
-  #Find neighbors and clusters WITH harmony batch correction
-  options(repr.plot.height = 2.5, repr.plot.width = 6)
-  T_NK_cell_subset <- T_NK_cell_subset %>% 
-    RunHarmony("sample", plot_convergence = TRUE)
-  #Find clusters
-  T_NK_cell_subset <- FindNeighbors(object = T_NK_cell_subset, dims = 1:41, reduction = "harmony")
-  T_NK_cell_subset <- FindClusters(object = T_NK_cell_subset, resolution = 1,  reduction = "harmony")
-  #Run UMAP
-  T_NK_cell_subset <- RunUMAP(object = T_NK_cell_subset, dims = 1:41, reduction = 'harmony')
-  DimPlot(object = T_NK_cell_subset, reduction = "umap", pt.size = 2, label = T)
-  
-  DotPlot(T_NK_cell_subset, features = unique(c(
-    "Cd3g","Cd3e","Cd4","Icos",
-    "Cd8a","Cd8b1",
-    "Foxp3","Klrg1","Ikzf2","Sigirr","Tnfrsf18","Gzmb","Nkg7","Ctla4")
-  )) + RotatedAxis()
-  
-  
-  #Assess Cluster Quality with FindAllMarkers Function
-  # T_cell.markers <- FindAllMarkers(T_NK_cell_subset)
-  # df = FindAllMarkers(T_NK_cell_subset)
-  # write.csv(file = "subset_Tcells.csv",x = df)
-  #Label the clusters
-  current.cluster.ids <- c(1:5)
-  current.cluster.ids[c(1,5)]="Treg"
-  current.cluster.ids[c(4)]="T4"
-  current.cluster.ids[c(2)]="T8"
-  current.cluster.ids[c(3)]="Other"
-  names(x = current.cluster.ids) <- levels(x = T_NK_cell_subset)
-  T_NK_cell_subset <- RenameIdents(object = T_NK_cell_subset, current.cluster.ids)
-  T_NK_cell_subset[["T_NK_cell_subset_labels"]]<- Idents(object = T_NK_cell_subset)
-  saveRDS(T_NK_cell_subset,file = paste0("GSE129455_T_NK_Viable_expression_",Sys.Date(),".rda"))
+    T_NK_cell_subset <- subset(TotalTissue.harmony, idents = "T")
+    
+    T_NK_cell_subset <- FindVariableFeatures(object = T_NK_cell_subset, mean.function = ExpMean, dispersion.function = LogVMR)
+    T_NK_cell_subset <- ScaleData(object = T_NK_cell_subset, vars.to.regress = c("nCount_RNA","CC.Difference"))
+    T_NK_cell_subset <- RunPCA(object = T_NK_cell_subset, pc.genes = T_NK_cell_subset@var.genes, do.print = TRUE, pcs.print = 1:5, genes.print = 5)
+    
+    #Find number of PCs that gives 90% variance
+    st_dev <- T_NK_cell_subset@reductions$pca@stdev
+    var <- st_dev^2
+    sum(var[1:41])/sum(var)
+    #Harmony Batch
+    #Find neighbors and clusters WITH harmony batch correction
+    options(repr.plot.height = 2.5, repr.plot.width = 6)
+    T_NK_cell_subset <- T_NK_cell_subset %>% 
+      RunHarmony("sample", plot_convergence = TRUE)
+    #Find clusters
+    T_NK_cell_subset <- FindNeighbors(object = T_NK_cell_subset, dims = 1:41, reduction = "harmony")
+    T_NK_cell_subset <- FindClusters(object = T_NK_cell_subset, resolution = 1,  reduction = "harmony")
+    #Run UMAP
+    T_NK_cell_subset <- RunUMAP(object = T_NK_cell_subset, dims = 1:41, reduction = 'harmony')
+    DimPlot(object = T_NK_cell_subset, reduction = "umap", pt.size = 2, label = T)
+    
+    DotPlot(T_NK_cell_subset, features = unique(c(
+      "Cd3g","Cd3e","Cd4","Icos",
+      "Cd8a","Cd8b1",
+      "Foxp3","Klrg1","Ikzf2","Sigirr","Tnfrsf18","Gzmb","Nkg7","Ctla4")
+    )) + RotatedAxis()
+    
+    
+    #Assess Cluster Quality with FindAllMarkers Function
+    # T_cell.markers <- FindAllMarkers(T_NK_cell_subset)
+    # df = FindAllMarkers(T_NK_cell_subset)
+    # write.csv(file = "subset_Tcells.csv",x = df)
+    #Label the clusters
+    current.cluster.ids <- c(1:5)
+    current.cluster.ids[c(1,5)]="Treg"
+    current.cluster.ids[c(4)]="T4"
+    current.cluster.ids[c(2)]="T8"
+    current.cluster.ids[c(3)]="Other"
+    names(x = current.cluster.ids) <- levels(x = T_NK_cell_subset)
+    T_NK_cell_subset <- RenameIdents(object = T_NK_cell_subset, current.cluster.ids)
+    T_NK_cell_subset[["T_NK_cell_subset_labels"]]<- Idents(object = T_NK_cell_subset)
+    saveRDS(T_NK_cell_subset,file = paste0("GSE129455_T_NK_Viable_expression_",Sys.Date(),".rda"))
 
   }
   
-  ##> CAF subset
+  # CAF subset
   {
     CAF_cell_subset <- subset(TotalTissue.harmony, idents = "CAF")
     
@@ -389,34 +386,38 @@ rm(list=ls())
     
   }
   
-  ##> merge subset
+  # merge subset
   {
-  rm(list=ls())  
-  TotalTissue.harmony = readRDS(file="GSE129455_All_Viable_annotate.rda")
-  T_NK_cell_subset = readRDS(file="GSE129455_T_NK_Viable_expression_2022-09-14.rda")
-  CAF_cell_subset = readRDS(file="GSE129455_CAF_Viable_expression_2022-09-14.rda")
-  
-  old_meta.df = TotalTissue.harmony@meta.data
-  old_meta.df$Main_subset_labels = as.character(old_meta.df$Main_cell_labels)
-  T_meta.df = T_NK_cell_subset@meta.data %>% dplyr::select(orig.ident,barcode,T_NK_cell_subset_labels)
-  CAF_meta.df = CAF_cell_subset@meta.data %>% dplyr::select(orig.ident,barcode,CAF_cell_subset_labels)
-  subset_meta.df = data.frame(orig.ident=c(T_meta.df$orig.ident,CAF_meta.df$orig.ident),
-                              barcode=c(T_meta.df$barcode,CAF_meta.df$barcode),
-                              subset_label=c(T_meta.df$T_NK_cell_subset_labels,CAF_meta.df$CAF_cell_subset_labels))
-  old_meta.df[subset_meta.df$orig.ident,"Main_subset_labels"]=as.character(subset_meta.df$subset_label)
-  old_meta.df$Main_subset_labels = factor(old_meta.df$Main_subset_labels,
-                                          levels=(c("PCC","Endo","Acinar","Pericyte","myCAF","iCAF","apCAF","T4","T8","Treg","B","MYE","NEU","MO","MAC","DC","Other")))
-  TotalTissue.harmony@meta.data = old_meta.df
-  
-  Idents(TotalTissue.harmony) = TotalTissue.harmony$Main_subset_labels
-  DimPlot(TotalTissue.harmony, reduction = "umap",label = TRUE, pt.size=1)
-  
-  saveRDS(TotalTissue.harmony,file = paste0("GSE129455_All_Viable_with_subset_",Sys.Date(),".rda"))
+    rm(list=ls())  
+    TotalTissue.harmony = readRDS(file="GSE129455_All_Viable_annotate.rda")
+    T_NK_cell_subset = readRDS(file="GSE129455_T_NK_Viable_expression_2022-09-14.rda")
+    CAF_cell_subset = readRDS(file="GSE129455_CAF_Viable_expression_2022-09-14.rda")
+    
+    old_meta.df = TotalTissue.harmony@meta.data
+    old_meta.df$Main_subset_labels = as.character(old_meta.df$Main_cell_labels)
+    T_meta.df = T_NK_cell_subset@meta.data %>% dplyr::select(orig.ident,barcode,T_NK_cell_subset_labels)
+    CAF_meta.df = CAF_cell_subset@meta.data %>% dplyr::select(orig.ident,barcode,CAF_cell_subset_labels)
+    subset_meta.df = data.frame(orig.ident=c(T_meta.df$orig.ident,CAF_meta.df$orig.ident),
+                                barcode=c(T_meta.df$barcode,CAF_meta.df$barcode),
+                                subset_label=c(T_meta.df$T_NK_cell_subset_labels,CAF_meta.df$CAF_cell_subset_labels))
+    old_meta.df[subset_meta.df$orig.ident,"Main_subset_labels"]=as.character(subset_meta.df$subset_label)
+    old_meta.df$Main_subset_labels = factor(old_meta.df$Main_subset_labels,
+                                            levels=(c("PCC","Endo","Acinar","Pericyte","myCAF","iCAF","apCAF","T4","T8","Treg","B","MYE","NEU","MO","MAC","DC","Other")))
+    TotalTissue.harmony@meta.data = old_meta.df
+    
+    Idents(TotalTissue.harmony) = TotalTissue.harmony$Main_subset_labels
+    DimPlot(TotalTissue.harmony, reduction = "umap",label = TRUE, pt.size=1)
+    
+    saveRDS(TotalTissue.harmony,file = paste0("GSE129455_All_Viable_with_subset_",Sys.Date(),".rda"))
   }
   
 }
-## Plot-All
+
+# Plot cell-type specific markers of shared cell types
+# NOT USED
 {
+  rm(list=ls())
+  TotalTissue.harmony = readRDS(file="GSE129455_All_Viable_annotate.rda")
   Epithelial = c("Epcam", "Krt19", "Krt8", "Krt18")
   Pericyte=c("Rgs5", "Acta2", "Pdgfrb")
   Fibroblast = c("Lum", "Dcn", "Col1a1", "Col3a1")
@@ -464,28 +465,4 @@ rm(list=ls())
   ) 
   print(p1)
   dev.off()
-}
-
-{
-  T_NK_cell_subset = readRDS(file="GSE129455_T_NK_Viable_expression_20220704.rda")
-  pdf(file="Vlnplot_KPC_T/NK.pdf",height = 15,width = 12)
-  p1 = VlnPlot(T_NK_cell_subset,features=c(
-    strsplit(paste(
-      "Klrg1,Tnfrsf18,Sigirr,Foxp3,Izumo1r,Pdcd1,Ikzf2,Tiam1,Uhrf2,Lztfl1,Rabgap1l",
-      #Epi
-      sep=","
-    ),split=",")[[1]]
-  ),pt.size = 0.000,ncol = 3,combine = TRUE
-  ) 
-  DotPlot(T_NK_cell_subset,features=c(
-    strsplit(paste(
-      "Klrg1,Tnfrsf18,Sigirr,Foxp3,Izumo1r,Pdcd1,Ikzf2,Tiam1,Uhrf2,Lztfl1,Rabgap1l",
-      #Epi
-      sep=","
-    ),split=",")[[1]]
-  )
-  ) 
-  print(p1)
-  dev.off()
-  
 }
